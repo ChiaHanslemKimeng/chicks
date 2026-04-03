5085a33f4b3172c8772414d704019596119accdd
Chia Hanslem Kimeng <hanslemkimeng5@gmail.com>
2026-04-03 20:03:40 +0100

    update3

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/<int:fowl_pk>/', views.checkout, name='checkout'),
+    path('checkout/<int:product_pk>/', views.checkout, name='checkout'),
     path('success/<int:order_id>/', views.order_success, name='order_success'),
 ]
