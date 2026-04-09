a2d1e82a3c814b5e8635d2dd6c656e4305ad6511
Chia Hanslem Kimeng <hanslemkimeng5@gmail.com>
2026-04-09 07:59:07 +0100

    update9

--- poultry_farm/settings.py
+++ poultry_farm/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-change-t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smc.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['universalpoultryfarm.com', 'smc.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
